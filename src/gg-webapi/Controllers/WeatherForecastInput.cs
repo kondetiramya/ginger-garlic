9b7109dfabb7a04f0dc0e42aee8967ee5818fd40
Ramya Kondeti <Ramya.Kondeti@hyland.com>
2021-05-07 14:14:20 -0400

    feat: added a post end-point to add new weather entries. A post end-point to add new weather entries has been added.

---
+++ src/gg-webapi/Controllers/WeatherForecastInput.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace gg_webapi.Controllers
+{
+    public class WeatherForecastInput
+    {
+        [Required]
+        [JsonPropertyName("TemperatureC")]
+        public int TemperatureC { get; }
+
+        [Required]
+        [JsonPropertyName("Summary")]
+        public string Summary { get; }
+
+        [JsonConstructor]
+        public WeatherForecastInput(int temperatureC, string summary)
+        {
+            TemperatureC = temperatureC;
+            Summary = summary;
+        }
+    }
+}
